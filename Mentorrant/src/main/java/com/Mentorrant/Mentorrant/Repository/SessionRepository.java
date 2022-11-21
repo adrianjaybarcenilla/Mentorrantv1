49f02b754e7eb0b9c3daa01a8b0910931e1ae6cd
John William Miones <johnwilliam.miones@cit.edu>
2022-11-22 02:17:54 +0800

    Add springframework

--- Mentorrant/src/main/java/com/Mentorrant/Mentorrant/Repository/SessionRepository.java
+++ Mentorrant/src/main/java/com/Mentorrant/Mentorrant/Repository/SessionRepository.java
@@ -1,9 +1,11 @@
 package com.Mentorrant.Mentorrant.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.Mentorrant.Mentorrant.Entity.SessionEntity;
 
+@Repository
 public interface SessionRepository extends JpaRepository <SessionEntity, Integer> {
 	SessionEntity findBySessionId(int sessionId);
 }
