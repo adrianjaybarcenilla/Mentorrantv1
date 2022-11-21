3cd8609baef07d788641d2dee1fa18d9e19e24b6
Estrella Abueva <estrella.abueva@cit.edu>
2022-11-21 21:07:46 +0800

    Initialize SpringBoot

---
+++ Mentorrant/src/main/java/com/Mentorrant/Mentorrant/MentorrantApplication.java
@@ -0,0 +1,13 @@
+package com.Mentorrant.Mentorrant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentorrantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentorrantApplication.class, args);
+	}
+
+}
