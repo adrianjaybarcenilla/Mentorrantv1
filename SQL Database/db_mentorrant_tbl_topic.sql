85254300c2f2b30cbf80dd73e7978c7513f5f329
Estrella Abueva <estrella.abueva@cit.edu>
2022-11-28 20:52:06 +0800

    Add changes for Topic Entity

--- SQL Database/db_mentorrant_tbl_topic.sql
+++ SQL Database/db_mentorrant_tbl_topic.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `db_mentorrant` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `db_mentorrant`;
 -- MySQL dump 10.13  Distrib 8.0.30, for Win64 (x86_64)
 --
 -- Host: localhost    Database: db_mentorrant
@@ -27,6 +25,7 @@ DROP TABLE IF EXISTS `tbl_topic`;
 CREATE TABLE `tbl_topic` (
   `topicid` int NOT NULL AUTO_INCREMENT,
   `subject` varchar(45) DEFAULT NULL,
+  `courseid` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`topicid`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -37,7 +36,7 @@ CREATE TABLE `tbl_topic` (
 
 LOCK TABLES `tbl_topic` WRITE;
 /*!40000 ALTER TABLE `tbl_topic` DISABLE KEYS */;
-INSERT INTO `tbl_topic` VALUES (2,'Appdev'),(3,'Automata'),(4,'IE'),(5,'IS2');
+INSERT INTO `tbl_topic` VALUES (2,'Appdev','321'),(3,'Automata','313'),(4,'IE','35'),(5,'IS2','32');
 /*!40000 ALTER TABLE `tbl_topic` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -50,4 +49,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-11-23 19:46:47
+-- Dump completed on 2022-11-28 20:50:56
